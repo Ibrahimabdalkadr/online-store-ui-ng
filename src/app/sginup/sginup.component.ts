f6f938b70e1e7e9ab7aea54be8dc11d7adf6a153
Ibrahimabdalkadr <ibrahemabdalkadr123@gmail.com>
2024-06-22 23:22:49 +0300

    generate application and add component

---
+++ src/app/sginup/sginup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sginup',
+  standalone: true,
+  imports: [],
+  templateUrl: './sginup.component.html',
+  styleUrl: './sginup.component.scss'
+})
+export class SginupComponent {
+
+}
